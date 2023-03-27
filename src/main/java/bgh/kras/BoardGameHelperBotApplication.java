dd989a9bce646a044287b33ce496bf71ff510566
VMishutkin <vladmish@list.ru>
2023-03-27 12:36:23 +0700

    Initial commit

---
+++ src/main/java/bgh/kras/BoardGameHelperBotApplication.java
@@ -0,0 +1,13 @@
+package bgh.kras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameHelperBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardGameHelperBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bgh/kras/BoardGameHelperBotApplicationTests.java
@@ -0,0 +1,13 @@
+package bgh.kras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameHelperBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
